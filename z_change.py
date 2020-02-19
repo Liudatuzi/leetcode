238e941c7ddfd86493ae2348daa70c32653edd25
Liudatuzi <46081997+Liudatuzi@users.noreply.github.com>
2020-02-20 00:16:25 +0800

    Update and rename z字形变换.py to z_change.py

--- z_change.py
+++ z_change.py
@@ -1,3 +1,14 @@
+# 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+
+# 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+
+# L   C   I   R
+# E T O E S I I G
+# E   D   H   N
+
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/zigzag-conversion
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 import numpy as np
 class Solution(object):
     def convert(self, s, numRows):
