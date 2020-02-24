e1245b0eafda40078cf3e369d9724b1297b9b606
Liudatuzi <46081997+Liudatuzi@users.noreply.github.com>
2020-02-24 20:52:41 +0800

    Create mergeKlists.py

---
+++ mergeKlists.py
@@ -0,0 +1,33 @@
+'''
+23. 合并K个排序链表
+合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+
+示例:
+
+输入:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+输出: 1->1->2->3->4->4->5->6
+'''
+#写的一肚子火气，我自己的虽然和这个看不出差别，但是就是不输出！！！！！！先把别人的贴上来。本来想写完这个写毕设。。。破题把我一天都搭进去了
+class Solution:
+    def mergeKLists(self, lists):
+        import heapq
+        dummy = ListNode(0)
+        p = dummy
+        head = []
+        for i in range(len(lists)):
+            if lists[i] :
+                heapq.heappush(head, (lists[i].val, i))
+                lists[i] = lists[i].next
+        while head:
+            val, idx = heapq.heappop(head)
+            p.next = ListNode(val)
+            p = p.next
+            if lists[idx]:
+                heapq.heappush(head, (lists[idx].val, idx))
+                lists[idx] = lists[idx].next
+        return dummy.next
